914ee7da3b670777c3ff8fda78ca7eef28854d08
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-01 23:38:08 +0900

    Firebaseでログイン機能開発途中

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { auth, provider } from "../firebase";
+import { signInWithPopup } from "firebase/auth";
 
 const Login = () => {
+	const loginWithGoogle = () => {
+		//Googleでログイン
+		signInWithPopup(auth, provider)
+			.then((result) => {
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			const credential = GoogleAuthProvider.credentialFromResult(result);
+			const token = credential.accessToken;
+			// The signed-in user info.
+			const user = result.user;
+			// IdP data available using getAdditionalUserInfo(result)
+			// ...
+		})
+	};
+
 	return (
-		<div>Login</div>
+		<div>
+			<p>ログインして始める</p>
+			<button onClick={loginWithGoogle}>Googleでログイン</button>
+		</div>
 	)
 }
 
