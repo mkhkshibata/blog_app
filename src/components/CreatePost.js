c73bb5efa8122774dee2173cfd6b3e8c709448da
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-05 00:37:14 +0900

    Firestoreにデータを保存する完成

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,12 +1,24 @@
 import React, { useState } from 'react';
+import { collection, addDoc } from 'firebase/firestore'
+import { auth, db } from '../firebase'
 import "./CreatePost.css";
+import { useNavigate } from 'react-router-dom';
 
 const CreatePost = () => {
 	const [title, setTitle] = useState("");
 	const [postText, setPostText] = useState();
+	const navigate = useNavigate();
 
-	const createPost = () => {
-		console.log(title, postText);
+	const createPost = async () => {
+		await addDoc(collection(db, "posts"), {
+			title: title,
+			postsText: postText,
+			auther: {
+				username: auth.currentUser.displayName,
+				id: auth.currentUser.uid
+			}
+		});
+		navigate("/");
 	}
 
 	return (
