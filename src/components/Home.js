bc88da8ced4547ae73c095a100b0555f0980aefa
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-05 20:22:34 +0900

    Homeコンポーネント作成

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+
+import './Home.css';
 
 const Home = () => {
+
+	useEffect(() => {
+		const getPosts = async () => {
+
+			const data = await getDocs(collection(db, "posts"));
+			console.log(data);
+			console.log(data.docs);
+			console.log(data.docs.map((doc) => {return { doc }} ));
+			console.log(data.docs.map((doc) => {return doc.data } ));
+
+		}
+		getPosts();
+	}, []);
+
+
 	return (
-		<div>Home</div>
+		<div className='homePage'>
+			<div className="postContents">
+				<div className="postHeader">
+					<h1>タイトル</h1>
+				</div>
+				<div className="postTextContainer">React学習中です。エンジニアとして活躍します、よろしくお願いします。React学習中です。エンジニアとして活躍します、よろしくお願いします。React学習中です。エンジニアとして活躍します、よろしくお願いします。React学習中です。エンジニアとして活躍します、よろしくお願いします。</div>
+				<div className="nameAndDeleteButton">
+					<h3>@mk</h3>
+					<button>削除</button>
+				</div>
+			</div>
+		</div>
 	)
 }
 
